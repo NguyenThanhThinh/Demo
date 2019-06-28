58446a019dee625dac70b80552e96fef27a5105f
Thinh Nguyen <thanhthinhcntt@gmail.com>
2019-06-28 13:36:51 +0700

    add

---
+++ demo-cusc/pages/contact.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="section is-medium">
+    <div class="container has-text-centered">
+      <h1 class="title">Contact Me</h1>
+      <p>
+        You can follow me on Github: <a href="https://github.com/nguyenthanhthinh" target="_blank">@ThinhNguyen</a>
+      </p>
+    </div>
+  </section>
+</template>
---
+++ demo-cusc/pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <section class="section is-large">
+    <div class="container has-text-centered">
+      <h1 class="title">Hi, I'm Thinh Nguyen</h1>
+      <h2 class="subtitle">a Software Developer of CUSC</h2>
+    </div>
+  </section>
+</template>
